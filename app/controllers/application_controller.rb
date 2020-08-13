d397aa2bcde841a7e6448a875ed3202b2229511c
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-08-13 10:38:23 -0500

    added helper methods for log ins and redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,45 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views' enable :sessions
+    set :views, 'app/views' 
+    enable :sessions
     set :session_secret, 'password_security'
   end
 
   get "/" do
-    erb :index
+    if !logged_in?
+      erb :index, :layout => : 'not_logged_in_layout'
+    else 
+      landing_page_redirect
+    end
   end
+
+  helpers do 
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def not_logged_in_redirect 
+      if !logged_in?
+        redirect "/login"
+      end 
+    end 
+
+    def landing_page_redirect 
+      redirect to "/posts"
+    end
+
+    def rooms_redirect 
+      redirect to "/rooms"
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+
+
+  
 
 end
