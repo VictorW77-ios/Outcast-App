8226d41f6024717e452e62b947773c4dff22d43e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-08-20 15:58:24 -0500

    removed spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-    has_secure_password 
     has_many :posts
+    
+    has_secure_password 
 end 
 
 ## inheriting from ActiveRecord does a lot of the initializing work for us.
