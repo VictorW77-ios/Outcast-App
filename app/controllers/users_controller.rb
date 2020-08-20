d56b8c300241fe24f437531be33b9742a3b4d79d
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-08-20 15:56:46 -0500

    created GET & POST methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
-# class UserController < ApplicationController
+class UsersController < ApplicationController
+
+    # render a login form
+    get "/login" do
+        erb :'users/login'
+    end
     
+    # recieves param data from login form
+    post "/login" do
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session["user_id"] = user.id
+            redirect to "/posts"
+        else 
+            # show an error message
+            redirect '/login'
+        end
+
+    end 
+
+
+end
 #     get '/signup' do 
 #         if Helpers.is_logged_in?(session)
 #             redirect to '/posts'
@@ -26,17 +46,6 @@
 #         erb :'users/login'
 #     end 
 
-#     post '/login' do
-#     user = User.find_by(:username => params[:username])
-#         if user && user.authenticate(params[:password])
-#             session["user_id"] = user.id
-#             redirect to "/posts"
-#         else 
-#             redirect to "/login"
-#         end
-
-#     end 
-
 #     get '/users/:slug' do
 #         @user = User.find_by_slug(params[:slug])
 #         if !@user.nil? 
@@ -56,6 +65,3 @@
 #         redirect to '/login'
 
 #     end 
-
-
-# end 
