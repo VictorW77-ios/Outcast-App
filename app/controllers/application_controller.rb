c95772201c356199c58f4940d82001c8f2bd851a
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-08-20 15:57:29 -0500

    removed comments + added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,48 +1,42 @@
-# require './config/environment'
+require './config/environment'
 
-# class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base
 
-#   configure do
-#     set :public_folder, 'public'
-#     set :views, 'app/views' 
-#     enable :sessions
-#     set :session_secret, 'password_security'
-#   end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views' 
+    enable :sessions
+    set :session_secret, 'password_security'
+  end
 
-#   get "/" do
-#     # if !logged_in?
-#     #   # erb :index, :layout => : 'not_logged_in_layout'
-#     # else 
-#     #   landing_page_redirect
-#     # end
-#   end
-
-#   helpers do 
-#     def logged_in?
-#       !!session[:user_id]
-#     end
-
-#     def not_logged_in_redirect 
-#       if !logged_in?
-#         redirect "/login"
-#       end 
-#     end 
-
-#     def landing_page_redirect 
-#       redirect to "/posts"
-#     end
-
-#     def rooms_redirect 
-#       redirect to "/rooms"
-#     end
-
-#     def current_user
-#       User.find(session[:user_id])
-#     end
-#   end
+  get "/" do
+    if logged_in?
+        redirect "users/#{current_user.id}"
+    else
+        erb :welcome
+    end
+  end 
 
 
 
   
+  helpers do 
 
-# end
+    def logged_in?
+        !!current_user
+    end 
+
+    def current_user 
+        User.find_by(id: session[:user_id])
+    end 
+
+    def edit_authorization?(post)
+        post.user == current_user
+    end 
+
+  end   
+
+
+
+end
+
