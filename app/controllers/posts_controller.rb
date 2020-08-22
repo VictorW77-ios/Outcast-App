f2e5760899560b2ada66f32b353ad4a16586da3b
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-08-22 10:07:30 -0500

    refactored routes + error messages + CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,28 +6,28 @@ class PostsController < ApplicationController
     # READING : model gets all posts from db
     get '/posts' do
         @posts = Post.all 
-        erb :'/posts/index'
+        erb :'posts/index'
     end 
 
     # CREATING : allows user to make a new post
     get '/posts/new' do
         if logged_in?
-            erb :'posts/new'
+            erb :'/posts/new'
         else 
-            # error message
-            redirect '/'
+            flash[:error] = "Log in to post!"
+            redirect "/"
         end 
     end 
 
     post '/posts' do
-        post = Post.new(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+        @post = Post.new(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
         
-        if post.save # varifies is input is legit, .save triggers validation
-            #need success message 
-            redirect "/posts/#{post.id}"
+        if @post.save # varifies is input is legit, .save triggers validation
+            flash[:message] = "Woohoo! Your post is up!"
+            redirect "/posts/#{@post.id}"
         else 
-            # need error message
-            redirect "/posts/new"
+            flash[:error] = "Couldn't create post [#{@post.errors.full_messages.to_sentence}]"
+            redirect "posts/new"
         end 
     end 
 
@@ -35,44 +35,37 @@ class PostsController < ApplicationController
     get '/posts/:id' do
         @post = Post.find(params[:id])
         
-        erb :'/posts/show'
+        erb :"/posts/show"
     end 
 
 
     # UPDATING : user can edit their post but no one else's
     get '/posts/:id/edit' do
-        @post = post.find(params[:id])
+        @post = Post.find(params[:id])
 
         if authorized_to_edit?(@post)
             erb :'/posts/edit'
         else 
-            # insert error message!!
-            redirect to '/posts'
+            flash[:error] = "You can't edit this post!"
+            redirect '/posts'
         end 
     end 
 
-    patch '/posts/id' do
+    patch '/posts/:id' do
         @post = Post.find(params[:id])
 
-        @post.update(title: params[:title], iamge_url: params[:image_url], description: params[:description])
+        @post.update(title: params[:title], image_url: params[:image_url], description: params[:description])
 
-        redirect to "/posts/#{@post.id}"
+        redirect "/posts/#{@post.id}"
     end 
 
-    # delete '/posts/:id/delete' do
-    #     @post = Post.find(params[:id])
-    #     @post.user =
-    #             @post = post/find_by_id(params[:id])
-    #             @post.delete 
-    #             redirect to '/posts'
-    #         else 
-    #             redirect to '/posts'
-    #         end
-    #     else 
-    #         redirect to '/login'
-    #     end
-        
-    # end 
+    # DELETING : uses id to find the post the user wants to delete
+    delete '/posts/:id' do
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect '/posts'
+    end 
 
 
 
